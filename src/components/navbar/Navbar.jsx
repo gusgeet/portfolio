e89e0cb6021bae709be7b635246c58bdad173857
gusgeet <gjgonzalez@hotmail.es>
2022-05-21 21:20:42 -0300

    m

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -6,14 +6,7 @@ import { css } from 'styled-components';
 
 const Navbar = () => {
     const [display, setDisplay] = useState(false);
-    const [status, setStatus] = useState('none')
     
-    const clickHandler = () => {
-        setDisplay(!display);
-        const seeStatus = display ? 'none' : 'grid'
-        setStatus(seeStatus)
-    }
-
 
 const NavbarContainer = styled.div`
     width: 100%;
@@ -109,7 +102,7 @@ return (
     <div>
         <NavbarContainer>
                 <NavbarWrap>
-                    <NavBtn onClick={() => clickHandler()}>
+                    <NavBtn onClick={() => setDisplay(!display)}>
                         <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
                             <path strokeLinecap="round" strokeLinejoin="round" d="M4 6h16M4 12h16M4 18h16" />
                         </svg>
